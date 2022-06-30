d43de1191d9401a97d47b6a9096e5917431834e9
Muhammad Hamza Saeed <muhammad.saeed@click.aero>
2022-06-30 16:52:22 +0400

    Refine plugins

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -4,46 +4,33 @@
  */
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js';
-import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
 import AutoImage from '@ckeditor/ckeditor5-image/src/autoimage.js';
-import AutoLink from '@ckeditor/ckeditor5-link/src/autolink.js';
 import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter.js';
-import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
-import Code from '@ckeditor/ckeditor5-basic-styles/src/code.js';
-import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock.js';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
 import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor.js';
 import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor.js';
 import FontFamily from '@ckeditor/ckeditor5-font/src/fontfamily.js';
 import FontSize from '@ckeditor/ckeditor5-font/src/fontsize.js';
-import GeneralHtmlSupport from '@ckeditor/ckeditor5-html-support/src/generalhtmlsupport.js';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
-import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight.js';
 import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline.js';
-import HtmlEmbed from '@ckeditor/ckeditor5-html-embed/src/htmlembed.js';
 import Image from '@ckeditor/ckeditor5-image/src/image.js';
+import ImageInsert from '@ckeditor/ckeditor5-image/src/imageinsert.js';
 import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize.js';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle.js';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar.js';
-import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
-import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock.js';
+import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
 import Link from '@ckeditor/ckeditor5-link/src/link.js';
-import LinkImage from '@ckeditor/ckeditor5-link/src/linkimage.js';
 import List from '@ckeditor/ckeditor5-list/src/list.js';
-import ListProperties from '@ckeditor/ckeditor5-list/src/listproperties.js';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
 import SpecialCharacters from '@ckeditor/ckeditor5-special-characters/src/specialcharacters.js';
+import SpecialCharactersCurrency from '@ckeditor/ckeditor5-special-characters/src/specialcharacterscurrency.js';
+import SpecialCharactersText from '@ckeditor/ckeditor5-special-characters/src/specialcharacterstext.js';
 import Table from '@ckeditor/ckeditor5-table/src/table.js';
-import TableCaption from '@ckeditor/ckeditor5-table/src/tablecaption.js';
-import TableCellProperties from '@ckeditor/ckeditor5-table/src/tablecellproperties';
 import TableColumnResize from '@ckeditor/ckeditor5-table/src/tablecolumnresize.js';
 import TableProperties from '@ckeditor/ckeditor5-table/src/tableproperties';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
-import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
-import Title from '@ckeditor/ckeditor5-heading/src/title.js';
-import TodoList from '@ckeditor/ckeditor5-list/src/todolist';
 import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
 
 class Editor extends ClassicEditor { }
@@ -51,46 +38,33 @@ class Editor extends ClassicEditor { }
 // Plugins to include in the build.
 Editor.builtinPlugins = [
 	Alignment,
-	Autoformat,
 	AutoImage,
-	AutoLink,
 	Base64UploadAdapter,
-	BlockQuote,
 	Bold,
-	Code,
-	CodeBlock,
 	Essentials,
 	FontBackgroundColor,
 	FontColor,
 	FontFamily,
 	FontSize,
-	GeneralHtmlSupport,
 	Heading,
-	Highlight,
 	HorizontalLine,
-	HtmlEmbed,
 	Image,
+	ImageInsert,
 	ImageResize,
 	ImageStyle,
 	ImageToolbar,
-	Indent,
-	IndentBlock,
+	ImageUpload,
 	Italic,
 	Link,
-	LinkImage,
 	List,
-	ListProperties,
 	Paragraph,
 	SpecialCharacters,
+	SpecialCharactersCurrency,
+	SpecialCharactersText,
 	Table,
-	TableCaption,
-	TableCellProperties,
 	TableColumnResize,
 	TableProperties,
 	TableToolbar,
-	TextTransformation,
-	Title,
-	TodoList,
 	Underline
 ];
 
@@ -100,32 +74,25 @@ Editor.defaultConfig = {
 		items: [
 			'heading',
 			'|',
-			'alignment',
-			'fontFamily',
-			'fontColor',
-			'fontBackgroundColor',
 			'fontSize',
+			'fontFamily',
+			'fontBackgroundColor',
+			'fontColor',
+			'|',
 			'bold',
 			'italic',
 			'link',
-			'underline',
+			'horizontalLine',
+			'|',
 			'bulletedList',
 			'numberedList',
-			'todoList',
 			'|',
-			'outdent',
-			'indent',
-			'|',
+			'imageUpload',
+			'imageInsert',
 			'insertTable',
-			'blockQuote',
 			'undo',
 			'redo',
-			'code',
-			'codeBlock',
-			'horizontalLine',
-			'highlight',
-			'htmlEmbed',
-			'specialCharacters'
+			'|'
 		]
 	},
 	language: 'en',
@@ -134,8 +101,7 @@ Editor.defaultConfig = {
 			'imageTextAlternative',
 			'imageStyle:inline',
 			'imageStyle:block',
-			'imageStyle:side',
-			'linkImage'
+			'imageStyle:side'
 		]
 	},
 	table: {
@@ -143,7 +109,6 @@ Editor.defaultConfig = {
 			'tableColumn',
 			'tableRow',
 			'mergeTableCells',
-			'tableCellProperties',
 			'tableProperties'
 		]
 	}
